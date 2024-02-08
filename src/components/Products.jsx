86d83fbb9b196631d00682b85f605590eeaaa8ba
medidipeddisai <saimedidi999@gmail.com>
2024-02-08 17:51:01 +0530

    your updated comments

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,6 +19,7 @@ const Products = () => {
                 setData(await response.clone().json());
                 setFilter(await response.json());
                 setLoading(false);
+                console.log(data)
             }
 
             return () => {
